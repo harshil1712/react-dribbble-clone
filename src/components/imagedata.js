b448fd4edcc42ad7e224d986573e1a60aa2c0a09
Harshil Agrawal <ghagrawal17@gmail.com>
2018-01-03 22:38:16 +0530

    basic modal created

--- src/components/imagedata.js
+++ src/components/imagedata.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Thumbnail} from 'react-bootstrap';
+import {Thumbnail, Modal,Button,Grid,Row,Col, Image} from 'react-bootstrap';
 import Slider from 'react-slick';
 
 const imgUrl = 'https://5a422991e1542700129be910.mockapi.io/imagecard/users/';
@@ -30,7 +30,8 @@ class Work extends Component{
     constructor(){
         super();
         this.state={
-            images:[]
+            images:[],
+            showModal:false
         }
     }
 
@@ -43,10 +44,23 @@ class Work extends Component{
         })
     }
 
+    getInitialState() {
+        return { showModal: false };
+    }
+
+    close =()=> {
+        this.setState({ showModal: false });
+    }
+
+    open= ()=> {
+        this.setState({ showModal: true });
+    }
+    
+
     render(){
         const images = this.state.images.map((item,i)=>(
             <div key={i}>
-                <Thumbnail src={item.imageUrl} key={i} />
+                <Thumbnail src={item.imageUrl} key={i} onClick={this.open} />
             </div>
         ));
         var settings = {
@@ -61,9 +75,44 @@ class Work extends Component{
 
         return(
             <div>
-            <Slider {...settings} style={{background:'transparent'}}>
-                {images}
-            </Slider>
+                <Slider {...settings} style={{background:'transparent'}}>
+                    {images}
+                </Slider>
+                <Modal show={this.state.showModal} onHide={this.close}>
+                    <Modal.Body>
+                        <Grid>
+                            <Row>
+                                <Col md={1}>
+                                    <Image src={this.props.image} alt='logo' circle className='img-logo' />
+                                </Col>
+                                <Col md={11}>
+                                    <h3>{this.props.name}</h3>
+                                </Col>
+                            </Row>
+                        </Grid>
+                        {/* <div className='container'> */}
+                            <h4>Text in a modal</h4>
+                            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+
+                            <h4>Popover in a modal</h4>
+                            <p>there is a <a href="#">popover</a>here</p>
+
+                            <h4>Tooltips in a modal</h4>
+                            <p>there is a  here</p>
+
+                            <h4>Overflowing text to show scroll behavior</h4>
+                            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+                            <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+                            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+                            <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+                            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+                            <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+                        {/* </div> */}
+                    </Modal.Body>
+                </Modal>
             </div>
         )
     }
